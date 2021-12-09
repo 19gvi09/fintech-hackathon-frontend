781afb92e38af6425a250d4ba9176a28715127ea
Anton2-7 <60310157+Anton2-7@users.noreply.github.com>
2021-12-09 10:22:40 +0500

    исправление_прыгающего_списка

--- src/components/Product/MenuRequirements.vue
+++ src/components/Product/MenuRequirements.vue
@@ -1,44 +1,61 @@
 <template>
-<div class="bloc1">
-  <ul class="nav">
-    <li>
-      <a href="#">Общие условия</a>
-      <ul>
-        <li><a href="#">пункт 1.1</a></li>
-        <li><a href="#">пункт 1.2</a></li>
-      </ul>
-    </li>
-    <li>
-      <a href="#">Ставка</a>
-      <ul>
-       
-      </ul>
-    </li>
-    <li>
-      <a href="#">Дополнительная сумма</a>
-      <ul>
-       
-      </ul>
-    </li>
-    <li>
-      <a href="#">Без подтверждения дохода</a>
-      <ul>
-       
-      </ul>
-    </li>
-    <li>
-      <a href="#">Требования к заемщику</a>
-      <ul>
-       
-      </ul>
-    </li>
-    <li>
-      <a href="#">Документы</a>
-      <ul>
-       
-      </ul>
-    </li>
-  </ul>
+  <div class="bloc1">
+    <ul class="nav">
+      <li>
+        <a href="#">Общие условия</a>
+        <ul>
+          <div class="left_block">
+            <div class="target_of_credit">
+              <p>
+                <span>Цель кредитования</span> - погашение ипотечного кредита
+                или займа,<br />обеспечаеного залогом квартиры или
+                залогомимущественных прав<br />по договору участия в долевом
+                строительстве, залогодержателем, <br />
+                по которому является банк или иная организация
+              </p>
+            </div>
+            <div class="value_of_credit">
+              <p>
+                <span>Сумма кредита:</span><br />Сумма кредита: — от 500 тыс. до
+                50 млн рублей для Москвы и МО, Санкт- <br />Петербурга и ЛО<br />
+                — от 500 тыс. до 20 млн рублей для других регионов РФ
+              </p>
+            </div>
+          </div>
+          <div class="right_block">
+            <p><span>Валюта кредита</span> - рубли РФ</p>
+            <p><span>Сроки кредитования</span> - от 3 до 30 лет</p>
+            <p><span>Валюта кредита</span> - рубли РФ</p>
+            <p>
+              <span>Размер кредита от стоимости залога:</span> От 20% до 70% -
+              есть или хотя бы один из ваших<br />
+              созаёмщиков являетесь ИП/владельцем (совладельцем) бизнеса <br />
+              - от 20% до 80% - в иных случаях
+            </p>
+          </div>
+        </ul>
+      </li>
+      <li>
+        <a href="#">Ставка</a>
+        <ul></ul>
+      </li>
+      <li>
+        <a href="#">Дополнительная сумма</a>
+        <ul></ul>
+      </li>
+      <li>
+        <a href="#">Без подтверждения дохода</a>
+        <ul></ul>
+      </li>
+      <li>
+        <a href="#">Требования к заемщику</a>
+        <ul></ul>
+      </li>
+      <li>
+        <a href="#">Документы</a>
+        <ul></ul>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -49,75 +66,156 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.bloc1 {
-    display: flex;
-    justify-content: center;
-    height: 410px;
+.left_block {
+  display: inline-block;
 }
-ul{
+.right_block {
+  display: inline-block;
+  padding-left: 50px;
+  span {
+    font-family: Lato, sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 19px;
+  }
+  p {
+    margin-top: 23px;
+  }
+  p ::before {
+    margin-left: -25px;
+    content: "";
+    background: #562cd3;
+    border-radius: 1ch;
+    padding-inline: 1ch;
+    margin-inline-end: 1ch;
+    height: 2px;
+    width: 2px;
+  }
+}
+.bloc1 {
+  display: flex;
+  justify-content: center;
+  height: 300px;
+}
+.value_of_credit {
+  margin-top: 30px;
+  span {
+    font-family: Lato, sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 19px;
+  }
+  ::before {
+    margin-left: -25px;
+    content: "";
+    background: #562cd3;
+    border-radius: 1ch;
+    padding-inline: 1ch;
+    margin-inline-end: 1ch;
+    height: 2px;
+    width: 2px;
+  }
+}
+.target_of_credit {
+  margin: 0;
+  color: #333333;
+
+  span {
+    font-family: Lato, sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 19px;
+  }
+  ::before {
+    margin-left: -25px;
+    content: "";
+    background: #562cd3;
+    border-radius: 1ch;
+    padding-inline: 1ch;
+    margin-inline-end: 1ch;
+    height: 2px;
+    width: 2px;
+  }
+}
+
+ul {
   margin: 0;
   padding: 0;
 }
-ul.nav li ul{
+ul.nav li ul {
   display: none;
-  
-}
-ul.nav li{
-  list-style: none;
-  margin: 30px;
-}
-ul.nav li:hover > ul{
-  display: block;
-  
 }
 ul.nav li {
-
-  float: left;                                   /*делаем меню горизонтальным*/
-  height: 30px;
-  list-style: none;  
+  list-style: none;
 }
+ul.nav li:hover > ul {
+  display: block;
+  p {
+    font-style: normal;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 19px;
+  }
+}
+ul.nav li {
+  float: left; /*делаем меню горизонтальным*/
+  height: 30px;
+  list-style: none;
+  width: 230px;
+}
+
 ul.nav li a {
   text-decoration: none;
   display: block;
   padding: 10px 10px 10px 10px;
-    color:black
-
+  color: black;
+  
 }
 ul.nav li ul {
-  display: none;	/*скрываем вложенные пункты*/
+  display: none; /*скрываем вложенные пункты*/
+  position: absolute;
+  left: 0;
+  right: 0;
 }
 ul.nav li:hover {
-background: #562CD3;
-/* DiForta_style/purple1 */
-border: 1px solid #562CD3;
-box-sizing: border-box;
-height: 40px;
-border-radius: 20px;
+  background: #562cd3;
+  /* DiForta_style/purple1 */
+  border: 1px solid #562cd3;
+  box-sizing: border-box;
+  height: 40px;
+  width: 230px;
+  border-radius: 20px;
 
 }
 ul.nav li a:hover {
-    color: #FFFFFF;
-   
+  color: #ffffff;
 }
 
-    /*Выпадающее меню*/
+/*Выпадающее меню*/
 ul.nav {
-  position: relative;
+  position: absolute;
   height: 30px;
-  margin:0 auto;
+  margin: 0 auto;
+  max-height: 60px;
+  
 }
 ul.nav li:hover > ul {
   border-top: 1px solid white;
-  display: block;
-  position: absolute;
-  top: 70px; 
-  width: 1230px;
-  height: 410px;
-  background: linear-gradient(180deg, #F9F9FC -2.41%, #FFFFFF 100%);
-/* sh2_arrow */
-
-box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.05);
-border-radius: 20px;
-  left: 40px;
+  display: flex;
+  justify-content: baseline;
+  width: 1220px;
+  height: 200px;
+  background: linear-gradient(180deg, #f9f9fc -2.41%, #ffffff 100%);
+  padding: 60px 10px 91px 93px;
+  text-align: left;
+  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.05);
+  border-radius: 20px;
+  display: absolute;
+  margin: 0 auto;
+  left: 0;
+  top:50px;
 }
 </style>
