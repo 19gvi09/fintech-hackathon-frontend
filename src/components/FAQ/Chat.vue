eaafc3347ceaed9887fe38e5b715088bcbf068d2
Anton2-7 <60310157+Anton2-7@users.noreply.github.com>
2021-12-10 21:15:08 +0500

    Добавлена_иконка_чата

---
+++ src/components/FAQ/Chat.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="chat">
+      <img src="../../assets/Ellipse.png" alt="Чат">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+};
+</script>
+
+<style lang="scss" scoped>
+.chat {
+  width: 60px;
+  height: 60px;
+  border-radius: 39px;
+  background: #ffffff;
+  box-shadow: -10px -10px 30px rgba(112, 32, 223, 0.05),
+    10px 10px 30px rgba(112, 32, 223, 0.05);
+  position: sticky;
+  bottom: 10px;
+  left:90%;
+  bottom:20px;
+  cursor: pointer;
+    display:flex;
+  align-items:center;
+  justify-content: center;
+}
+</style>
--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,19 +1,22 @@
 <template>
     <HeadImage />
     <All />
+    <Chat/>
 </template>
 
 <script>
 
 import HeadImage from "../components/FAQ/HeadImage.vue";
 import All from "../components/FAQ/All.vue";
+import Chat from "../components/FAQ/Chat.vue";
 
 
 export default {
     name: 'FAQ',
      components: {
         HeadImage,
-        All
+        All,
+        Chat
     }
 }
 </script>
