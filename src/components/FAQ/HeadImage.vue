740b074e0e0dea3714f81370a5ed37e2608f1761
Anton2-7 <60310157+Anton2-7@users.noreply.github.com>
2021-12-10 17:51:39 +0500

    Доваблен_блок_head

---
+++ src/components/FAQ/HeadImage.vue
@@ -0,0 +1,20 @@
+<template>
+    <container>
+        <img src="../../assets/head.png" alt="head">
+    </container>
+</template>
+
+
+<script>
+export default {
+    name: "HeadImage",
+}
+</script>
+
+<style lang="scss" scoped>
+container {
+    width: 1440px;
+    height: 150px;
+}
+</style> 
+
---
+++ src/views/FAQ.vue
@@ -0,0 +1,21 @@
+<template>
+    <HeadImage />
+</template>
+
+<script>
+
+import HeadImage from "../components/FAQ/HeadImage.vue";
+
+
+export default {
+    name: 'FAQ',
+     components: {
+        HeadImage,
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
